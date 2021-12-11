4a78c3ccf8bbe00d4e5abbc34cbc176996f077d4
Marin <marin.teskera2002@gmail.com>
2021-12-11 07:17:52 +0100

    day11

---
+++ Day11/1.py
@@ -0,0 +1,44 @@
+f = open('Day11/input.txt', 'r')
+
+
+def flash(grid, i, j, flashed, flashes):
+    if flashed.count((i, j)) != 0:
+        return
+    if grid[i][j] > 9:
+        flashed.append((i, j))
+        flashes += 1
+        for (i, j) in [(i-1, j-1), (i-1, j), (i-1, j+1), (i, j-1), (i, j+1), (i+1, j-1), (i+1, j), (i+1, j+1)]:
+            if 0 <= i < 10 and 0 <= j < 10:
+                grid[i][j] += 1
+                if grid[i][j] > 9 and flashed.count((i, j)) == 0:
+                    flash(grid, i, j, flashed, flashes)
+
+
+lines = f.readlines()
+f.close()
+
+grid = []
+flashes = 0
+flashed = []
+
+for line in lines:
+    lista = []
+    for i in line:
+        if i != '\n':
+            lista.append(int(i))
+    grid.append(lista)
+
+for i in range(100):
+    flashed = []
+    for j in range(len(grid)):
+        for k in range(len(grid[j])):
+            grid[j][k] += 1
+            if grid[j][k] > 9:
+                flash(grid, j, k, flashed, flashes)
+    for j in range(len(grid)):
+        for k in range(len(grid[j])):
+            if grid[j][k] > 9:
+                grid[j][k] = 0
+    flashes += len(flashed)
+
+print("Flashes:", flashes)
